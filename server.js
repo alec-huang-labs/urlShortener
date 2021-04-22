1b7b3cfd7f359df77d105fb937e7c5b7b19e1c8e
Alec Huang <alec@Alecs-MacBook-Pro.local>
2021-04-22 13:41:21 -0400

    added new api endpoints

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require("body-parser")
+const validUrl = require("valid-url")
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -15,16 +19,42 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl/new', function(req, res){
-  res.json()
+
+let shortUrl = 0;
+let originalUrl = "";
+var httpsRegex = /http/
+
+app.post('/api/shorturl', function(req, res){
+  console.log(req.body)
+  originalUrl = req.body.url
+  shortUrl = Math.floor(Math.random()*1000)
+  console.log(shortUrl)
+  if (!httpsRegex.test(originalUrl)){
+      console.log(originalUrl)
+      res.json({ error: 'invalid url' })
+  } else {
+    if(validUrl.isUri(originalUrl)){
+      console.log('is a valid url')
+      res.json({
+        original_url: originalUrl, 
+        short_url: shortUrl
+      })
+    } else {
+      res.json({ error: 'invalid url' })
+    }
+  }
 })
 
+app.get('/api/shorturl/:shortUrlRegex', function(req,res){
+  if (shortUrlRegex = shortUrl){
+    res.redirect(originalUrl)
+  }
+})
+ 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
-//go to http://localhost:3000
-
